2e4a9648cc9e6c4d65857d718b75debff91012bb
Olaf Lüke <olaf@tinkerforge.com>
2014-03-17 00:41:32 +0100

    Add Edge Count, Edge Type and Debounce options to Hall Effect Plugin

--- js/brickv/plugins/PluginBrickletHallEffect.js
+++ js/brickv/plugins/PluginBrickletHallEffect.js
@@ -8,7 +8,7 @@ function PluginBrickletHallEffect() {
   this.init = function() {
     this.he = null;
     this.lastValue = 0;
-    this.graph = new BrickGraph(this.updatePeriod, this.maxPoints, this.getValue.bind(this), 'Values', ' ', 'halleffect-bricklet');
+    this.graph = new BrickGraph(this.updatePeriod, this.maxPoints, this.getValue.bind(this), 'Value', ' ', 'hall-effect-bricklet');
   };
 
   this.setDeviceInformation = function(deviceInformation) {
@@ -20,10 +20,37 @@ function PluginBrickletHallEffect() {
     $('#dashboard').append(html);
     
     this.graph.addDOMElements($('#dashboard'));
+    
+    htmlEdgeCount = '<div class="col-xs-16 col-sm-12"><h3>Edge Count: <b id="hall-effect-bricklet-edge-count"></b></h3></div>'; 
+    $('#dashboard').append(htmlEdgeCount);
+    
+    htmlEdgeType = '<div class="col-xs-8 col-sm-4">Edge detection type: <select id="hall-effect-bricklet-edge-type" class="form-control">' +
+      '<option value="0">Rising Edge</option>' +
+      '<option value="1">Falling Edge</option>' +
+      '<option value="2">Both</option>' +
+      '</select></div>';
+    $('#dashboard').append(htmlEdgeType);
+    
+    htmlDebounce = '<div class="col-xs-8 col-sm-4">Debounce:<input class="col-md-8 form-control" type="text" id="hall-effect-bricklet-debounce"/></div>';
+    $('#dashboard').append(htmlDebounce);
   };
   
-  this.getValue = function () {
+  this.getValue = function() {
     return this.lastValue;
+  };
+  
+  this.writeConfig = function() {
+    var edgeTypeValue = parseInt($("#hall-effect-bricklet-edge-type option:selected").val(), 10);
+    if(edgeTypeValue !== 0 && edgeTypeValue !== 1 && edgeTypeValue !== 2) {
+      return;
+    }
+    
+    var debounce = parseInt($('#hall-effect-bricklet-debounce').val(), 10);
+    if(value < 0 || value > 255) {
+      return
+    }
+    
+    this.he.setEdgeCountConfig(edgeTypeValue, debounce);
   };
 
   this.start = function() {
@@ -32,28 +59,58 @@ function PluginBrickletHallEffect() {
       
       this.addDOMElements();
       this.running = true;
-
+      
+      $("#hall-effect-bricklet-edge-type").click(this.writeConfig.bind(this));
+      $('#hall-effect-bricklet-debounce').on('touchspin.on.stopupspin', this.writeConfig.bind(this));
+      $('#hall-effect-bricklet-debounce').on('touchspin.on.stopdownspin', this.writeConfig.bind(this));
+      
       this.he = new Tinkerforge.BrickletHallEffect(this.deviceInformation.uid, brickViewer.ipcon);
-      this.he.getValue(
-        function(edge_count, value) {
-          if (value == true) {
-		    this.lastValue = 1;
-		  } else {
-		    this.lastValue = 0;
-		  }
-          this.graph.start();
-        }.bind(this)
-      );
+      this.he.getValue(function(value) {
+        if (value === true) {
+  		    this.lastValue = 1;
+  		  } else {
+  		    this.lastValue = 0;
+  		  }
+        this.graph.start();
+      }.bind(this));
+      
+      this.he.getEdgeCount(false, function(edgeCount) {
+        $('#hall-effect-bricklet-edge-count').text(edgeCount);
+      });
+      
+      this.he.getEdgeCountConfig(function(edgeType, debounce) {
+        $('#hall-effect-bricklet-edge-type option').removeAttr('selected');
+        switch(edgeType) {
+          case Tinkerforge.BrickletHallEffect.EDGE_TYPE_RISING:
+            $("#hall-effect-bricklet-edge-type option[value='0']").attr('selected', true);
+            break;
+          case Tinkerforge.BrickletHallEffect.EDGE_TYPE_FALLING:
+            $("#hall-effect-bricklet-edge-type option[value='1']").attr('selected', true);
+            break;
+          case Tinkerforge.BrickletHallEffect.EDGE_TYPE_BOTH:
+            $("#hall-effect-bricklet-edge-type option[value='2']").attr('selected', true);
+            break;
+        }
+        
+        $('#hall-effect-bricklet-debounce').TouchSpin({
+          min: 0,
+          max: 255,
+          stepinterval: 1,
+          maxboostedstep: 5,
+          initval: debounce,
+          postfix: 'ms'
+        });
+      });
       
       this.he.on(Tinkerforge.BrickletHallEffect.CALLBACK_EDGE_COUNT,
-        function(edge_count, value) {
-		  if (value == true) {
-		    this.lastValue = 1;
-		  } else {
-		    this.lastValue = 0;
-		  }
-        }.bind(this)
-      );
+        function(edgeCount, value) {
+    		  if (value === true) {
+    		    this.lastValue = 1;
+    		  } else {
+    		    this.lastValue = 0;
+    		  }
+        $('#hall-effect-bricklet-edge-count').text(edgeCount);
+      }.bind(this));
 
       this.he.setEdgeCountCallbackPeriod(this.updatePeriod);
     }
